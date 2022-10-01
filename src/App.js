154cc0a9fe9bb2e2a94d17c178882dcde204f44c
victips <torvicarias@gmail.com>
2022-10-01 12:15:39 -0500

    Add styles and content

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Faq from "./components/faq/Faq";
+import Footer from "./components/footer/Footer"
+
 const App = () => {
   return (
     <div>
       <Faq />
+      <Footer />
     </div>
   );
 }
