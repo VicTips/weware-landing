9c217044bf83fc93bec08e3a3d17c2923fb7e2fa
victips <torvicarias@gmail.com>
2022-10-01 19:13:10 -0500

    Add styles

--- src/components/faq/Faq.js
+++ src/components/faq/Faq.js
@@ -4,13 +4,13 @@ import QuestionsList from "../../services/constants/QuestionsList"
 const Faq = () => {
   return (
     <section className='px-12 mb-24'>
-      <h2 className='text-5xl font-extrabold text-center mb-[0.25rem] text-indigo-600'>Frequently Asked Questions</h2>
+      <h2 className='text-5xl font-extrabold text-center mb-5 text-indigo-600'>Frequently Asked Questions</h2>
       <div className='flex justify-center'>
         <div className='max-w-[463px] text-[17px] text-neutral-800'>
           With lots of unique blocks, you can easily build a page without coding. Build your next landing page.
         </div>
       </div>
-      <div className='grid grid-cols-1 sm:grid-cols-2 mt-10'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 mt-14 px-48'>
         {QuestionsList.map((question, index) => (
           <div key={index} className='p-5'>
             <div className='font-bold text-indigo-600 text-xl'>{question.question}</div>
