be9426cb2f457f60a892cba7dc4a88b299741894
victips <torvicarias@gmail.com>
2022-10-02 12:45:27 -0500

    Add content and styles

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Features from "./components/features/Features";
 import Reviews from "./components/reviews/Reviews"
 import Faq from "./components/faq/Faq";
 import Footer from "./components/footer/Footer"
+import Teams from "./components/teams/Teams";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Teams />
       <Features />
       <Reviews />
       <Faq />
---
+++ src/components/teams/Teams.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Teams = () => {
+  return (
+    <div className='bg-gray-100 my-36'>
+      <div className='flex items-center justify-evenly py-12 px-20'>
+        <div>
+          <h2 className='text-indigo-600 font-extrabold text-5xl max-w-[468px]'>Remote teams love using Wikit</h2>
+          <div className='max-w-[391px] text-stone-600'>
+            Your entire team in one place together, with Engage your team with company announcements that entice engagement.
+          </div>
+        </div>
+        <img src='https://res.cloudinary.com/ds90phfgd/image/upload/v1664669334/wikit/Teams_dygxna.png' alt='teams' />
+      </div>
+    </div>
+  )
+}
+
+export default Teams
